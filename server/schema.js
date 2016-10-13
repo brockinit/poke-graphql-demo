3ad94e8155ba7aa1cc9d357bdef2cd08bb187805
Brock Lanoza <brock.lanoza@hpe.com>
2016-10-12 20:50:24 -1000

    Add PokemonType

--- server/schema.js
+++ server/schema.js
@@ -2,23 +2,42 @@ import {
 	GraphQLSchema,
 	GraphQLObjectType,
 	GraphQLString,
+  GraphQLList,
+  GraphQLInt,
 } from 'graphql';
 
+// Define custom type for querying pokemon
+const PokemonType = new GraphQLObjectType({
+  name: 'PokemonType',
+  fields: () => {
+    return {
+      trainer_id: GraphQLInt,
+      poke_name: GraphQLString,
+      poke_type: GraphQLString,
+      hp: GraphQLInt,
+      attack: GraphQLInt,
+    };
+  },
+});
 
 // This is where all of our querying will stem from
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
-  
-  fields: {
-    hello: {
-      type: GraphQLString,
-      resolve: () => 'world',
-    },
-  }
+  fields: () => {
+    return {
+      pokemon: {
+        type: new GraphQLList(PokemonType),
+        args: {
+          trainerId: { type: GraphQLInt },
+        },
+        resolve: () => 'world',
+      },
+    };
+  },
 });
 
 const pokeSchema = new GraphQLSchema({
-  query: RootQueryType
+  query: RootQueryType,
   // ...mutation
 });
 
