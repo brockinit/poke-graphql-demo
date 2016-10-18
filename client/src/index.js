3d9233cee7fcde238b0cce7879b22916727c2463
Brock Lanoza <brock.lanoza@hpe.com>
2016-10-17 21:22:18 -1000

    Add material-ui

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import './index.css';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 const client = new ApolloClient({
   networkInterface: createNetworkInterface('http://localhost:8000/graphql'),
@@ -11,7 +14,9 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
   </ApolloProvider>,
   document.getElementById('root')
 );
