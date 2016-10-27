63424c577ddf1455c3f835cc249b4bc4e1928951
Brock Lanoza <brock.lanoza@hpe.com>
2016-10-26 19:01:05 -1000

    Add pokemon to trainer query

--- client/src/queries/trainerQuery.js
+++ client/src/queries/trainerQuery.js
@@ -2,10 +2,17 @@ import gql from 'graphql-tag';
 
 export const trainerQuery = gql`
   query {
-  	trainers {
-    	id
-    	first_name
-    	last_name
-  	}
+    trainers {
+      id
+      first_name
+      last_name
+      pokemon {
+        trainer_id
+        poke_name
+        poke_type
+        hp
+        attack
+      }
+    }
   }
 `;
